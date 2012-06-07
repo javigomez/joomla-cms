b271142ec7d5fcebaaf56ea04b0da0087cbaebf2
Javier <javier.gomez.corio@gmail.com>
2012-06-06 19:55:38 -0600

    Update SQL for Joomla to add the MySQL Accredited Translations Update Site
    This SQL adds the update site of the Accredited Joomla! Translations list to #__update_sites table. Note that the SQL uses null as "update_site_id".
    The SQLAzure file is empty just to remind that will need to be filled.

---
+++ administrator/components/com_admin/sql/updates/mysql/2.5.X.sql
@@ -0,0 +1,34 @@
+# Insert the update server for Accredited translations if not exist
+INSERT INTO
+	`#__update_sites` ( update_site_id, name, type, location, enabled, last_check_timestamp )
+SELECT 
+	null, 
+	'Accredited Joomla! Translations', 
+	'collection', 
+	'http://update.joomla.org/language/translationlist.xml', 
+	1, 
+	0
+FROM `#__update_sites`
+WHERE NOT EXISTS (
+	SELECT 
+		* 
+	FROM 
+		`#__update_sites` 
+	WHERE 
+		location = 'http://update.joomla.org/language/translationlist.xml');
+
+# Link english lang and the Accredited translations update site for JUpdater needs
+INSERT INTO 
+	`#__update_sites_extensions` 
+VALUES
+	(
+		(
+			SELECT 
+				update_site_id 
+			FROM 
+				`#__update_sites` 
+			WHERE 
+				location = 'http://update.joomla.org/language/translationlist.xml'
+		)
+		, 600
+	);
