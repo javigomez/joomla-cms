1449ea5145d235b4edc306db70e46cb6893e6610
Javier <javier.gomez.corio@gmail.com>
2012-06-06 19:13:43 -0600

    Joomla Update SQL to add the Accredited Translations update site
    the #__update_sites table stores the location of the list of accredited translations. This manifest is needed for the installer to work. This SQL adds the update site to the database.

---
+++ administrator/components/com_admin/sql/updates/mysql/2.5.X.sql
@@ -0,0 +1,5 @@
+INSERT INTO `#__update_sites` VALUES
+(null, 'Accredited Joomla! Translations', 'collection', 'http://update.joomla.org/language/translationlist.xml', 1, 0);
+
+INSERT INTO `#__update_sites_extensions` VALUES
+(LAST_INSERT_ID(), 600)
