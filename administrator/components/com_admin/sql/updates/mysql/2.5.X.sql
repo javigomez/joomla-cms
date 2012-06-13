4fa4b802aea06171d8353d487c661c6d56240704
Javier <javier.gomez.corio@gmail.com>
2012-06-13 12:04:05 -0600

    Solving problem of update_server getting id 0
    The update server of accredited translation could get the ID 0 causing problems.
    
    This new SQL sets its id to 10000 and the auto increment to 10001. That ensures not to override existing update servers in the #__update_sites table.
    
    And also leave plenty of space to Joomla Core to add specific update servers in between 2 and 9999 update_servers

--- administrator/components/com_admin/sql/updates/mysql/2.5.X.sql
+++ administrator/components/com_admin/sql/updates/mysql/2.5.X.sql
@@ -1,34 +1,17 @@
 # Insert the update server for Accredited translations if not exist
-INSERT INTO
-	`#__update_sites` ( update_site_id, name, type, location, enabled, last_check_timestamp )
-SELECT 
-	null, 
-	'Accredited Joomla! Translations', 
-	'collection', 
-	'http://update.joomla.org/language/translationlist.xml', 
-	1, 
-	0
-FROM `#__update_sites`
-WHERE NOT EXISTS (
-	SELECT 
-		* 
-	FROM 
-		`#__update_sites` 
-	WHERE 
-		location = 'http://update.joomla.org/language/translationlist.xml');
-# Link english lang and the Accredited translations update site for JUpdater needs
-# TODO insert not strict
 INSERT IGNORE INTO
-	`#__update_sites_extensions` 
+    `#__update_sites`
 VALUES
-	(
-		(
-			SELECT 
-				update_site_id 
-			FROM 
-				`#__update_sites` 
-			WHERE 
-				location = 'http://update.joomla.org/language/translationlist.xml'
-		)
-		, 600
-	);
+    (10000,
+    'Accredited Joomla! Translations',
+     'collection',
+     'http://update.joomla.org/language/translationlist.xml',
+     1,
+     0);
+
+# Link english lang and the Accredited translations update site for JUpdater needs
+INSERT IGNORE INTO
+    `#__update_sites_extensions`
+VALUES
+	(10000,
+	600);
