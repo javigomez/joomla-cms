f1b8f8e4e56a18c5707396d9451d47423da77ef4
Javier <javier.gomez.corio@gmail.com>
2012-06-14 11:47:14 -0600

    SQL for SQLServer
    This is the SQLServer joomla update schema file. I will need to be tested on a Azure environment.

--- administrator/components/com_admin/sql/updates/sqlazure/2.5.x.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/2.5.x.sql
@@ -1,3 +1,19 @@
 # Insert the update server for Accredited translations if not exist
 
-# TODO
+SET IDENTITY_INSERT #__update_sites  ON;
+
+INSERT INTO #__update_sites (update_site_id, name, type, location, enabled, last_check_timestamp)
+SELECT 10000, 'Accredited Joomla! Translations', 'collection', 'http://update.joomla.org/language/translationlist.xml', 1, 0;
+
+SET IDENTITY_INSERT #__update_sites  OFF;
+
+
+
+# Link english lang and the Accredited translations update site for JUpdater needs
+
+SET IDENTITY_INSERT #__update_sites_extensions  ON;
+
+INSERT INTO #__update_sites_extensions (update_site_id, extension_id)
+SELECT 10000, 600;
+
+SET IDENTITY_INSERT #__update_sites_extensions  OFF;
