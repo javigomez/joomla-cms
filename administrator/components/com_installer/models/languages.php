1333b3b3b67fa21fe378f800d79561d374d4b092
Javier <javier.gomez.corio@gmail.com>
2012-07-30 15:16:43 -0600

    fix a bad use of JUpdate

--- administrator/components/com_installer/models/languages.php
+++ administrator/components/com_installer/models/languages.php
@@ -132,7 +132,7 @@ class InstallerModelLanguages extends JModelList
 		 * The following function uses extension_id 600, that is the english language extension id.
 		 * In #__update_sites_extensions you should have 600 linked to the Accredited Translations Repo
 		 */
-		$updater->findUpdates(600, $cache_timeout);
+		$updater->findUpdates(array(600), $cache_timeout);
 
 		return true;
 	}
