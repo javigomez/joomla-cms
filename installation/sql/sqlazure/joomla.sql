00807706a07da27b04aecd0ac7159ae8b3e0d787
Javier <javier.gomez.corio@gmail.com>
2012-07-30 13:21:01 -0600

    Update joomla-sql
    The changes updates the joomla database to be able to find the language update server at http://update.joomla.org.
    
    There are modified files for updating to joomla 2.5.7 and changes into the joomla.sql for fresh installs.
    
    The files are for MySQL and SQLServer.

--- installation/sql/sqlazure/joomla.sql
+++ installation/sql/sqlazure/joomla.sql
@@ -6462,9 +6462,23 @@ SELECT 8, 1, 18, 19, 'Super Users';
 
 SET IDENTITY_INSERT #__usergroups  OFF;
 
+/* database changes for version 2.5.7 */
+/* Insert the update server for Accredited translations if not exist */
+INSERT IGNORE INTO
+    `#__update_sites`
+VALUES
+    (10000,
+    'Accredited Joomla! Translations',
+     'collection',
+     'http://update.joomla.org/language/translationlist.xml',
+     1,
+     0);
 
-
-
-
+/* Link english lang and the Accredited translations update site for JUpdater needs */
+INSERT IGNORE INTO
+    `#__update_sites_extensions`
+VALUES
+    (10000,
+    600);
 
 
